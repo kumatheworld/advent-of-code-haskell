4a316acbd24d2c6de62c82344b8c29426e77752b
Yoshihiro Kumazawa <kum4th3w0rld@gmail.com>
2025-12-01 21:16:02 +0000

    Remove Day01Spec reference from test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,7 @@
 import Test.Hspec
 
--- Import day test modules here as they are created
-import qualified Day01Spec
-
 main :: IO ()
 main = hspec $ do
   describe "Advent of Code Tests" $ do
     it "placeholder test" $ do
       True `shouldBe` True
-    
-    -- Add day test suites here as they are created
-    Day01Spec.spec
