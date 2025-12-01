135212077b19d86ad4f0001e5457a86207ea83f0
Yoshihiro Kumazawa <kum4th3w0rld@gmail.com>
2025-12-01 20:45:55 +0000

    Add Haskell program to download puzzle inputs

---
+++ app/Download.hs
@@ -0,0 +1,33 @@
+module Main (main) where
+
+import Network.HTTP.Simple
+import qualified Data.ByteString.Char8 as BS
+import System.Environment (getArgs)
+import System.Exit (die)
+import Text.Printf (printf)
+import Control.Exception (catch, SomeException)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [dayStr, year] -> downloadInput (read dayStr) (read year)
+    _ -> die "Usage: cabal run download <day> <year>"
+
+downloadInput :: Int -> Int -> IO ()
+downloadInput day year = do
+  sessionCookie <- BS.readFile "session.cookie"
+  let url = printf "https://adventofcode.com/%d/day/%d/input" year day
+      outputFile = printf "data/inputs/%02d.txt" day
+  
+  request <- parseRequest url
+  let request' = setRequestHeader "Cookie" [BS.pack "session=" <> BS.strip sessionCookie] request
+  
+  catch (do
+    response <- httpBS request'
+    BS.writeFile outputFile (getResponseBody response)
+    putStrLn $ "Downloaded input for day " ++ show day ++ " to " ++ outputFile
+    ) (\e -> do
+      let err = show (e :: SomeException)
+      die $ "Failed to download input: " ++ err ++ "\n\nMake sure session.cookie exists with your AoC session token."
+    )
