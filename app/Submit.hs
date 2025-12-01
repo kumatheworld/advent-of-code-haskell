c7bb7f317934a7b65938240f2aaa45b04e4e93d2
Yoshihiro Kumazawa <kum4th3w0rld@gmail.com>
2025-12-01 21:19:08 +0000

    Add solution submission program

---
+++ app/Submit.hs
@@ -0,0 +1,47 @@
+module Main (main) where
+
+import Network.HTTP.Simple
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.CaseInsensitive as CI
+import System.Environment (getArgs)
+import System.Exit (die)
+import Text.Printf (printf)
+import Control.Exception (catch, SomeException)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [dayStr, partStr, answer, year] -> submitSolution (read dayStr) (read partStr) answer (read year)
+    _ -> die "Usage: cabal run submit <day> <part> <answer> <year>"
+
+submitSolution :: Int -> Int -> String -> Int -> IO ()
+submitSolution day part answer year = do
+  sessionCookie <- BS.readFile "session.cookie"
+  let url = printf "https://adventofcode.com/%d/day/%d/answer" year day
+      body = BS.pack $ "level=" ++ show part ++ "&answer=" ++ answer
+  
+  request <- parseRequest url
+  let request' = setRequestMethod "POST"
+               $ setRequestHeader (CI.mk $ BS.pack "Cookie") [BS.pack "session=" <> BS.strip sessionCookie]
+               $ setRequestHeader (CI.mk $ BS.pack "Content-Type") [BS.pack "application/x-www-form-urlencoded"]
+               $ setRequestBodyBS body
+               $ request
+  
+  catch (do
+    response <- httpBS request'
+    let responseBody = BS.unpack $ getResponseBody response
+    
+    if "That's the right answer" `elem` words responseBody
+      then putStrLn "✓ Correct answer!"
+      else if "That's not the right answer" `elem` words responseBody
+        then putStrLn "✗ Wrong answer"
+        else if "You gave an answer too recently" `elem` words responseBody
+          then putStrLn "⏳ Please wait before submitting again"
+          else if "Did you already complete it" `elem` words responseBody
+            then putStrLn "Already completed"
+            else putStrLn $ "Response: " ++ take 200 responseBody
+    ) (\e -> do
+      let err = show (e :: SomeException)
+      die $ "Failed to submit: " ++ err
+    )
