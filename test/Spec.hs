694d26c23e6e036568b8c280fd6bdcf82145c56e
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-04 13:02:09 +0000

    Remove things specific to Day 1

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,14 +2,12 @@ import Test.Hspec
 
 -- Auto-generated imports will be inserted below this marker by the scaffold command
 -- AUTOGEN-IMPORTS
-import qualified Day01
 
 main :: IO ()
 main = hspec $ do
   describe "Advent of Code Tests" $ do
     -- Auto-generated per-day tests will be inserted below this marker by scaffold
     -- AUTOGEN-TESTS
-    Day01.tests
 
     it "placeholder test" $ do
       True `shouldBe` True
